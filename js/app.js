ef5cec18964d3ab4ef52c08a86b7d60a6969a5fd
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-25 22:01:05 +0000

    refactor: Add event listener to both pictures

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-const img = document.querySelector('.main-img'),
-			counter = document.querySelector('.clicks-counter');
+const images = document.querySelectorAll('.main-img'),
+			counter = document.querySelector('.clicks-counter'),
+			cat1Name = document.querySelector('#cat1 h2');
+			cat2Name = document.querySelector('#cat2 h2');
 
 let clicks = 0;
 
-img.addEventListener('click', function() {
-	clicks++;
-	counter.innerText = clicks;
+catsNames = {
+	cat1: 'James',
+	cat2: 'Jorge'
+}
+
+cat1Name.innerText = catsNames.cat1;
+cat2Name.innerText = catsNames.cat2;
+
+images.forEach( element => {
+	element.addEventListener('click', function() {
+		clicks++;
+		counter.innerText = clicks;
+	});
 });
