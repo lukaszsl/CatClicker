a6deac8f41fb55fd882499a882c2266aa1d3aaac
lukaszsl <lukasz.sliczner@interia.pl>
2018-04-26 23:51:14 +0000

    refactor: Rebuild event listeners

--- js/app.js
+++ js/app.js
@@ -2,29 +2,24 @@ const listContainer = document.querySelector('.list');
 const list = document.createElement('ol');
 const links = document.querySelectorAll('a');
 const catImg = document.querySelector('.cat__img');
+const clickCounter = document.querySelector('.clicks-counter');
 
 listContainer.appendChild(list);
 
-function Cat(name, pictureUrl) {
+function Cat(name, pictureUrl, number, counter=0) {
 	this.name = name;
 	this.pictureUrl = pictureUrl;
+	this.number = number;
+	this.counter = counter;
 }
 
-let cat1 = new Cat('James', 'img/cat1.jpg');
-let cat2 = new Cat('Tim', 'img/cat2.jpg');
-let cat3 = new Cat('Sam', 'img/cat3.jpg');
-let cat4 = new Cat('Sandy', 'img/cat4.jpg');
-let cat5 = new Cat('Roger', 'img/cat5.jpg');
+let cat1 = new Cat('James', 'img/cat1.jpg', 1);
+let cat2 = new Cat('Tim', 'img/cat2.jpg', 2);
+let cat3 = new Cat('Sam', 'img/cat3.jpg', 3);
+let cat4 = new Cat('Sandy', 'img/cat4.jpg', 4);
+let cat5 = new Cat('Roger', 'img/cat5.jpg', 5);
 
 const cats = [cat1, cat2, cat3, cat4, cat5];
-
-let images = [
-	'img/cat1.jpg',
-	'img/cat2.jpg',
-	'img/cat3.jpg',
-	'img/cat4.jpg',
-	'img/cat5.jpg'
-];
 
 // Add links to the list
 for (let i=0; i<5; i++) {
@@ -35,21 +30,16 @@ for (let i=0; i<5; i++) {
 }
 
 // Add event listener to list nodes. Main cat image source get path from images array.
-	list.addEventListener('click', function(e) {
-		catImg.src = images[e.target.value];
-		console.log(e.target.value)
-	});
+list.addEventListener('click', function(e) {
+	catImg.src = cats[e.target.value].pictureUrl;
+	clickCounter.textContent = cats[e.target.value].counter;
+});
 
-
-
-// images.forEach( element => {
-// 	element.addEventListener('click', function(e) {
-// 		if(e.target.id === "cat1-img") {
-// 			clicksCounter1++;
-// 			counters[0].innerText = clicksCounter1;
-// 		} else if(e.target.id === "cat2-img") {
-// 			clicksCounter2++;
-// 			counters[1].innerText = clicksCounter2;
-// 		}
-// 	});
-// });
+catImg.addEventListener('click', function(e) {
+	for(let i=0; i<cats.length; i++) {
+		if(`file:///Users/lukaszsliczner/Desktop/udacity/FEND/exercises/CatClicker/${cats[i].pictureUrl}` === e.target.src) {
+			cats[i].counter++;
+			clickCounter.textContent = cats[i].counter;
+		}
+	}
+});
